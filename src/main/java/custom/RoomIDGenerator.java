dfe17a4fb68dda4285170302dc439ea853137ab3
SeemaNadagouda <SeemaNadagouda@gmail.com>
2019-01-22 18:45:16 +0530

    crud issues user entity

--- src/main/java/custom/RoomIDGenerator.java
+++ src/main/java/custom/RoomIDGenerator.java
@@ -8,15 +8,22 @@ import org.hibernate.HibernateException;
 import org.hibernate.Query;
 import org.hibernate.engine.spi.SharedSessionContractImplementor;
 import org.hibernate.id.IdentifierGenerator;
+import org.meeting.pro.Entity.Rooms;
 
 public class RoomIDGenerator implements IdentifierGenerator {
-
+	static int y=1;
 	public Serializable generate(SharedSessionContractImplementor session, Object object) throws HibernateException {
 		// TODO Auto-generated method stub
 		String prefix = "ROOM_";
 		//Query q =  session.createNamedQuery("current_count");
 	//	Integer count = (Integer) q.uniqueResult();
-		prefix= prefix+0;
+		Rooms current = (Rooms)object;
+		if(current.getR_id()!= null)
+		{
+			return current.getR_id();
+		}
+		prefix= prefix+y;
+		y++;
 		System.out.println("returning "+prefix);
 		return prefix;
 	}
--- src/main/java/org/meeting/pro/Controllers/AdminController.java
+++ src/main/java/org/meeting/pro/Controllers/AdminController.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -13,13 +14,13 @@ import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.mvc.method.annotation.HttpEntityMethodProcessor;
 
 @RestController
-@RequestMapping("/admin")
+@RequestMapping("/rooms")
 public class AdminController {
 @Autowired
 RoomService rs;
 	
 	@RequestMapping(value = "/createRoom" , method = RequestMethod.POST)
-	public ResponseEntity<Rooms> createaRoom(@RequestBody Rooms newroom) {
+	public ResponseEntity<Rooms> createRoom(@RequestBody Rooms newroom) {
 		System.out.println("Receivedreques is => "+newroom);
 		return rs.createRoom(newroom);
 		//ResponseEntity<Rooms> rt = new ResponseEntity<Rooms>(newroom,HttpStatus.OK);
@@ -27,14 +28,26 @@ RoomService rs;
 		
 		
 	}
-	
-	@RequestMapping(value = "/check" , method = RequestMethod.GET)
-	public ResponseEntity<String> checkmate() {
-		
-		return new ResponseEntity<String>("ABC",HttpStatus.ACCEPTED);
-		//ResponseEntity<Rooms> rt = new ResponseEntity<Rooms>(newroom,HttpStatus.OK);
-		// return rt;
-		
+	@RequestMapping(value = "/getRoom/{rid}" , method = RequestMethod.GET)
+	public ResponseEntity<Rooms> getRoom(@PathVariable("rid") String rid ) {
+		System.out.println("Receivedreques is => "+rid);
+		return rs.getRoom(rid);
 		
 	}
+	@RequestMapping(value = "/deleteRoom/{rid}" , method = RequestMethod.DELETE)
+	public ResponseEntity<Rooms> deleteRoom(@PathVariable("rid") String rid ) {
+		System.out.println("Receivedreques is => "+rid);
+		return rs.deleteRoom(rid);
+		
+	}
+	@RequestMapping(value = "/updateRoom" , method = RequestMethod.PUT)
+	public ResponseEntity<Rooms> updateRoom(@RequestBody Rooms newroom) {
+		System.out.println("Receivedreques is => "+newroom);
+		return rs.updateRoom(newroom);
+		
+	}
+	
+	
+	
+	
 }
